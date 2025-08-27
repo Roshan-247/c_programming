32ed58b0417c1c55b5f4a3b4e75d6169ee76f7f3
Roshan-247 <roshanparikh247@gmail.com>
2025-08-27 17:15:45 +0530

    Create dollorintorupees.c

---
+++ dollorintorupees.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+int dollors, rupees; 
+printf("Enter dollors"); 
+scanf("%d",&dollors);
+rupees = dollors * 48;
+printf("% dollors = %d rupees", dollors, rupees);
+}
